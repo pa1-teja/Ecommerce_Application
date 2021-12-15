17bbde5e4d288d59bf12298b30bf4e89d2951f4c
KONA VENKATA SAI PAVAN TEJA <pa1-teja@users.noreply.github.com>
2021-12-15 20:52:52 +0530

    initial commit

---
+++ src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -0,0 +1,39 @@
+package com.example.demo.model.requests;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CreateUserRequest {
+
+	@JsonProperty
+	private String username;
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	@JsonProperty
+	private String confirmPassword;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@JsonProperty
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
---
+++ src/test/java/com/example/demo/Controllers/UserControllerTest.java
@@ -0,0 +1,108 @@
+package com.example.demo.Controllers;
+
+import com.example.demo.Controllers.TestUtils;
+import com.example.demo.controllers.UserController;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class UserControllerTest {
+
+    private UserController userController;
+
+    private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+
+
+    @Mock
+    private UserRepository userRepo =mock(UserRepository.class);
+
+    @Mock
+    private CartRepository cartRepo =mock(CartRepository.class);
+
+    @Before
+    public void setup() {
+        userController = new UserController();
+        TestUtils.injectObjects(userController, "userRepository",userRepo);
+        TestUtils.injectObjects(userController, "bCryptPasswordEncoder",encoder);
+        TestUtils.injectObjects(userController, "cartRepository", cartRepo);
+    }
+
+    @Test
+    public void createUserTest(){
+            when(encoder.encode("testPassword")).thenReturn("thisIsHashed");
+            CreateUserRequest r = new CreateUserRequest();
+            r.setUsername("test");
+            r.setPassword("testPassword");
+            r.setConfirmPassword("testPassword");
+
+            final ResponseEntity<User> response = userController.createUser(r);
+
+            assertNotNull(response);
+            assertEquals(200, response.getStatusCodeValue());
+
+            User u = response.getBody();
+            assertNotNull(u);
+            assertEquals(0, u.getId());
+            assertEquals("test", u.getUsername());
+            assertEquals("thisIsHashed", u.getPassword());
+    }
+
+    @Test
+    public void checkPasswordSpecification(){
+        CreateUserRequest r = new CreateUserRequest();
+        r.setUsername("test");
+        r.setPassword("abcde");
+        r.setConfirmPassword("abcde");
+
+        final ResponseEntity<User> response = userController.createUser(r);
+        assertEquals(400, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void test_find_by_userName(){
+        CreateUserRequest req = new CreateUserRequest();
+        req.setUsername("test");
+        req.setPassword("abc");
+        req.setConfirmPassword("abc");
+        User u= new User();
+        u.setUsername("test");
+        u.setId(1);
+        when(userRepo.findByUsername(Matchers.anyString())).thenReturn(u);
+        ResponseEntity<User> userRepsonse= userController.findByUserName("test");
+        assertEquals("test",userRepsonse.getBody().getUsername());
+
+    }
+
+    @Test
+    public void test_find_by_userId(){
+        CreateUserRequest req = new CreateUserRequest();
+        req.setUsername("test");
+        req.setPassword("abc");
+        req.setConfirmPassword("abc");
+        User u= new User();
+        u.setUsername("test");
+        u.setId(Long.valueOf(1));
+        when(userRepo.findById(Matchers.anyLong())).thenReturn(java.util.Optional.of(u));
+        ResponseEntity<User> userRepsonse= userController.findById(Long.valueOf(1));
+        assertEquals(1l,userRepsonse.getBody().getId());
+
+    }
+
+
+}
